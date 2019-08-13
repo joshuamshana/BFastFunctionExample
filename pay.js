60ede77d896d105f4c84e16a30820100496b1fa8
Joshua Mshana <mama27j@gmail.com>
2019-08-13 11:42:57 +0300

    Update pay.js

--- pay.js
+++ pay.js
@@ -5,8 +5,3 @@ exports.pay  = (req, res)=>{
 exports.verifyPay = function(req, res){
     res.json({message: 'verify pay'});
 }
-
-exports.test = (req, res)=>{
-     res.json({message: 'test function works'});
-}
-
